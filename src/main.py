2161a774d29f46937c5e6a3cbfa92b299f24cc37
João Sereia <joao.lo.sereia@gmail.com>
2024-03-22 21:33:34 -0300

    add main function

--- src/main.py
+++ src/main.py
@@ -4,41 +4,46 @@ from snake import Snake
 from food import Food
 
 
-pygame.init()
+def main():
+    pygame.init()
 
-screen = pygame.display.set_mode(consts.screen)
-clock = pygame.time.Clock()
-running = True
+    screen = pygame.display.set_mode(consts.screen)
+    clock = pygame.time.Clock()
+    running = True
 
-food = Food(screen)
-snake = Snake(
-    food=food,
-    screen=screen,
-    position=(consts.screen[0] / 2, consts.screen[1] / 2),
-    velocity=(0, 0),
-    pixels=[],
-    size=1,
-)
+    food = Food(screen)
+    snake = Snake(
+        food=food,
+        screen=screen,
+        position=(consts.screen[0] / 2, consts.screen[1] / 2),
+        velocity=(0, 0),
+        pixels=[],
+        size=1,
+    )
 
-while running:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-        elif event.type == pygame.KEYDOWN:
-            snake.dir(event.key)
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+            elif event.type == pygame.KEYDOWN:
+                snake.dir(event.key)
 
-    screen.fill(consts.bg_color)
+        screen.fill(consts.bg_color)
 
-    food.paint()
+        food.paint()
 
-    snake.move()
-    snake.gen()
-    snake.paint()
+        snake.move()
+        snake.gen()
+        snake.paint()
 
-    pygame.display.update()
+        pygame.display.update()
 
-    snake.eat()
+        snake.eat()
 
-    clock.tick(consts.fps)
+        clock.tick(consts.fps)
 
-pygame.quit()
+    pygame.quit()
+
+
+if __name__ == "__main__":
+    main()
